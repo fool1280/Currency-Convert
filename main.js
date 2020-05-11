a43d791e15751f2d65967b0ad97c28cc29f4f19e
fool1280 <anhnguyen@Anhs-MBP.attlocal.net>
2020-05-11 02:56:44 -0500

    add prompt

--- main.js
+++ main.js
@@ -24,14 +24,14 @@ function formatVND(type, value) {
   return formatter.format(value);
 }
 
-//let currency = prompt("Choose your currency VND, USD, or EUR?");
+let currency = prompt("Choose your currency VND, USD, or EUR?");
 
-let currency = "VND"
+//let currency = "VND"
 
 //== is comparing type && value, === is comparing value
 //NaN != NaN: this is true
 if (currency === "VND") {
-  let amountVND = 100000; //prompt("How much VND?");
+  let amountVND = prompt("How much VND?");
   let check_number = parseInt(amountVND);
   if (Object.is(check_number, NaN)) {
     console.log("Please input a number!");
@@ -40,7 +40,7 @@ if (currency === "VND") {
     console.log("From VND to USD:", formatUSD('en-IN',res_vndToUsd));  
   }
 } else if (currency === "USD") {
-  let amountUSD = 1; //prompt("How much USD?");
+  let amountUSD = prompt("How much USD?");
   let check_number = parseInt(amountUSD);
   if (Object.is(check_number, NaN)) {
     console.log("Please input a number!");
